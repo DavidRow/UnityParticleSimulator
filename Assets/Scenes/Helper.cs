ae200e807ab17601fbd11148f009156f5b74fbf4
David Rowson <davidfr333@vt.edu>
2021-08-16 15:45:46 -0400

    added more

--- Assets/Scenes/Helper.cs
+++ Assets/Scenes/Helper.cs
@@ -10,7 +10,7 @@ public static class  Helper {
         return Texture;
     }
     //make a list of agents 
-    public static Agent[] makeAgents(int numAgents, int width,int  height ){
+    public static Agent[] makeAgents(int numAgents, int width,int  height, Vector4 color ){
         Agent[] agents = new Agent[numAgents];
         float halfwidth = width/2;
         float halfheight = height/2;
@@ -18,11 +18,11 @@ public static class  Helper {
         for(int i =0; i < numAgents; i++){
 			Vector2 startPos = new Vector2(halfwidth, halfheight);
 			float randomAngle = UnityEngine.Random.value * Mathf.PI * 2;
-            agents[i] = new Agent() {position = startPos, angle = randomAngle };
+            agents[i] = new Agent() {position = startPos, angle = randomAngle, color = color };
         }
         return agents;
     }
-    public static Agent[] makeAgentsInCircle(int numAgents, int width,int  height){
+    public static Agent[] makeAgentsInCircle(int numAgents, int width,int  height, Vector4 color){
         Agent[] agents = new Agent[numAgents];
         float halfwidth = width/2;
         float halfheight = height/2;
@@ -31,20 +31,26 @@ public static class  Helper {
             Vector2 startPosCircle = Random.insideUnitCircle * 10;
 			Vector2 startPos = new Vector2(halfwidth + startPosCircle.x , halfheight+ startPosCircle.y);
 			float randomAngle = UnityEngine.Random.value * Mathf.PI * 2;
-            agents[i] = new Agent() {position = startPos, angle = randomAngle };
+            agents[i] = new Agent() {position = startPos, angle = randomAngle, color = color  };
         }
         return agents;
     }
-    public static Agent[] makeAgentsInCircleAngleCenter(int numAgents, int width,int  height, int CircleRadius){
+    public static Agent[] makeAgentsInCircleAngleCenter(int numAgents, int width,int  height, int CircleRadius, Vector4[] color){
+        int length = color.Length;
+        int index = 0;
         Agent[] agents = new Agent[numAgents];
         float halfwidth = width/2;
         float halfheight = height/2;
-
+        
         for(int i =0; i < numAgents; i++){
             Vector2 startPosCircle = Random.insideUnitCircle * CircleRadius;
 			Vector2 startPos = new Vector2(halfwidth + startPosCircle.x , halfheight+ startPosCircle.y);
 			float AngleToCenter = getAngleBetween(startPos.x,startPos.y,halfwidth,halfheight);
-            agents[i] = new Agent() {position = startPos, angle = AngleToCenter };
+            if(index >=  length){
+                index = 0;
+            }
+            agents[i] = new Agent() {position = startPos, angle = AngleToCenter, color = color[index]};
+            index++;
         }
         return agents;
     }
--- Assets/ShaderProject.cs
+++ Assets/ShaderProject.cs
@@ -3,35 +3,41 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public struct Agent{ public Vector2 position; public float angle; }
+public struct Agent{ public Vector2 position; public float angle;public Vector4 color; }
+public struct Person {public Vector2 position; public int health; }
+
 public class ShaderProject : MonoBehaviour {
 
     //setup stuff
     public ComputeShader ParticleShader;
     public RenderTexture ParticleTexture;
-    public int followON = 0;
+    public int followON = 1;
 
     ComputeBuffer agentsBuffer;
 
     private Agent[] agents;
     private int KIdrawAgents = 1;
     private int KIdarken = 0;
-    private int numAgents = 10000;
-    private int width = 200;
-    private int height = 200;
+    private int numAgents = 100000;
+    private int width = 1000;   
+    private int height = 1000;
     private int XthreadsAgents = 0;
     int Xthreadsdarken = 0;
     int Ythreadsdarken = 0;
-
-
+    Vector4 color1 = new Vector4(1 , 0 , 0 , 0);
+    Vector4 color2 = new Vector4(0 , 1 , 0 , 0);
+    //Vector4 color3 = new Vector4(0 , 0 , 1 , 0);
+    //Vector4 color4 = new Vector4(1 , 1 , 1 , 0);
+    
     void Start(){
 
         //make the texture
         ParticleTexture = Helper.makeTexture(ParticleTexture, width, height);
         
         //make a list of agents
-        agents =  Helper.makeAgents(numAgents, width, height);
-        //agents = Helper.makeAgentsInCircleAngleCenter(numAgents, width, height, 50 );
+        //agents =  Helper.makeAgents(numAgents, width, height, color);
+        Vector4[] colors = new Vector4[] {color1, color2};
+        agents = Helper.makeAgentsInCircleAngleCenter(numAgents, width, height, 500, colors );
 
         //get the IDs of the shaders
         KIdrawAgents = ParticleShader.FindKernel("drawAgents");
@@ -40,6 +46,7 @@ public class ShaderProject : MonoBehaviour {
         //set all the var to the shaders
         ParticleShader.SetInt("width", width);
         ParticleShader.SetInt("height", height);
+        ParticleShader.SetInt("numAgents", numAgents);
         int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Agent));
         agentsBuffer = new ComputeBuffer(numAgents,stride);
         agentsBuffer.SetData(agents);
